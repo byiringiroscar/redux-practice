d970de511f0651b3074cdb50d2ab35bbba336de2
byiringiroscar <byiringoroscar@gmail.com>
2023-04-12 12:50:16 +0200

    Add redux flow with state, action, dispatch, subscribe and reducer

---
+++ index.js
@@ -0,0 +1,54 @@
+const redux = require('redux')
+
+const createStore = redux.createStore
+const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+
+
+function orderCake(){
+    return {
+        type: CAKE_ORDERED,
+        payload: 1,
+    }
+}
+function restockCake(qty=1){
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,      
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,  
+}
+
+// (previousState, action) => newState
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state, 
+                numOfCakes: state.numOfCakes - 1
+            }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes  + action.payload
+            }
+        default:
+            return state
+}
+}
+
+const store = createStore(reducer)
+console.log('initial state', store.getState());
+const unsubscribe = store.subscribe(() => console.log('Update state', store.getState()))
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+
+store.dispatch(restockCake(3)) 
+
+unsubscribe()
